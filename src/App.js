ee219eace46d773b277f8f21fc4e07c120494f8e
Alex Williams <alexwilliams567@gmail.com>
2017-12-15 17:38:32 -0500

    Adding react-router and express files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,119 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './css/App.css';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import AppBar from 'material-ui/AppBar';
-import RaisedButton from 'material-ui/RaisedButton';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./css/App.css";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import AppBar from "material-ui/AppBar";
+import RaisedButton from "material-ui/RaisedButton";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Drawer from "material-ui/Drawer";
+import MenuItem from "material-ui/MenuItem";
 
 const style = {
-  margin: 12,
+  margin: 12
 };
 
-const App = () => (
-  <MuiThemeProvider>
-    <AppBar
-      title="Voting App"
-      iconElementRight={<RaisedButton label="Log in with Twitter" secondary={true} style={style} />}
-    />
-  </MuiThemeProvider>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    };
+  }
+
+  handleToggle = () =>
+    this.setState({
+      open: !this.state.open
+    });
+
+  handleClose = () =>
+    this.setState({
+      open: false
+    });
+
+  render() {
+    return (
+      <Router>
+        <MuiThemeProvider>
+          <AppBar
+            title="Voting App"
+            onLeftIconButtonClick={this.handleToggle}
+            iconElementRight={
+              <RaisedButton
+                label="Log in with Twitter"
+                secondary={true}
+                style={style}
+              />
+            }
+          />
+
+          <Drawer
+            docked={false}
+            width={200}
+            open={this.state.open}
+            onRequestChange={open => this.setState({ open })}
+          >
+            <MenuItem>
+              <Link to="/" onClick={this.handleClose}>
+                Home
+              </Link>
+            </MenuItem>
+            <MenuItem>
+              <Link to="/about" onClick={this.handleClose}>
+                About
+              </Link>
+            </MenuItem>
+            <MenuItem>
+              <Link to="/topics" onClick={this.handleClose}>
+                Topics
+              </Link>
+            </MenuItem>
+          </Drawer>
+          <Route exact="exact" path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/topics" component={Topics} />
+        </MuiThemeProvider>
+      </Router>
+    );
+  }
+}
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
 );
 
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+);
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+);
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>Components</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.url}/:topicId`} component={Topic} />
+    <Route
+      exact="exact"
+      path={match.url}
+      render={() => <h3>Please select a topic.</h3>}
+    />
+  </div>
+);
 
 export default App;
